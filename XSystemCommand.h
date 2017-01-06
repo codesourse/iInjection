84bd293dbcb3dd97195ecfd2a95b4f444a1394e1
xiakj <1135768777@qq.com>
2017-01-06 14:08:07 +0800

    Add files via upload

---
+++ XSystemCommand.h
@@ -0,0 +1,15 @@
+//
+//  XSystemCommand.h
+//  iOS App Signer
+//
+//  Created by jsb-xiakj on 2016/12/23.
+//  Copyright © 2016年 Daniel Radtke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XSystemCommand : NSObject
+-(void)rystemCommand:(NSString *)cmd;
+-(int )contentDataPath:(NSString *)dataPath;
+-(void)fileString:(NSString *)pathSting  newDisplayName:(NSString *)display;
+@end
